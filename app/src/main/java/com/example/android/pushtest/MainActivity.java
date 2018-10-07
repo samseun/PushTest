52e8440d05ecb1d5ca1593d45041bc18ac912bb8
Samehase <samsuenmail@gmail.com>
2018-10-07 19:21:17 +0100

    So... this is the first thing im pushing to github

---
+++ app/src/main/java/com/example/android/pushtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.pushtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ThisBetterWork{
+        int testing = 12;
+    }
+}
